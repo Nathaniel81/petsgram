9eb744ddc6ece4e05a2e94fbb6bdc572b89189fd
Nathaniel81 <nathanield356@gmail.com>
2024-08-25 12:45:14 +0300

    install nativewind

---
+++ Petsgram/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+	<View className='flex flex-1 justify-center items-center'>
+	  <Text className='font-pbold'>index</Text>
+	</View>
+  )
+}
+
+export default index
