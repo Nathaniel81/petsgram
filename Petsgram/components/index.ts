caeb3b3059a6d30e70fab74c11aa02b1ad022786
Nathaniel81 <nathanield356@gmail.com>
2024-08-28 16:09:14 +0300

    initiate home page layout and structure

---
+++ Petsgram/components/index.ts
@@ -0,0 +1,13 @@
+import PostCard from "./PostCard";
+import FormField from "./FormField";
+import CustomButton from "./CustomButton";
+import Loader from "./Loader";
+import EmptyState from "./EmptyState";
+
+export {
+  PostCard,
+  FormField,
+  CustomButton,
+  Loader,
+  EmptyState,
+};
